86e55493944c151f889d04cd780c8bc22052e313
fdetsch <florian.detsch@staff.uni-marburg.de>
2015-03-23 11:59:36 +0100

    Initial upload: function that returns available files per SLS research site (mrg, mrg_rf, mrg_rf_agg01h) and corresponding season.

---
+++ R/slsAvlFls.R
@@ -0,0 +1,32 @@
+slsAvlFls <- function(ch_path = "../../phd/scintillometer/data", 
+                      ch_pattern = c("mrg.csv$", "mrg_rf.csv$", 
+                                     "mrg_rf_agg01h.csv$")) {
+  
+  ls_df_sls_fls <- lapply(ch_pattern, function(tmp_ch_pattern) {
+    tmp_ch_fls <- list.files(ch_path, full.names = TRUE,
+                             pattern = tmp_ch_pattern, recursive = TRUE)
+    
+    # current plot
+    ch_sls_plt <- basename(tmp_ch_fls)
+    ls_sls_plt <- strsplit(ch_sls_plt, "_")
+    ch_sls_plt <- sapply(ls_sls_plt, "[[", 1)
+    
+    ch_sls_hab <- substr(ch_sls_plt, 1, 3)
+    
+    # current season
+    int_sls_ds <- which(duplicated(ch_sls_plt))
+    ch_sls_ssn <- rep("r", length(ch_sls_plt))
+    ch_sls_ssn[int_sls_ds] <- "d"
+    
+    ch_process_level <- substr(tmp_ch_pattern, 1, nchar(tmp_ch_pattern)-5)
+    tmp_df_fls <- data.frame(ch_sls_plt, ch_sls_hab, ch_sls_ssn, tmp_ch_fls, 
+                             stringsAsFactors = FALSE)
+    names(tmp_df_fls) <- c("plot", "habitat", "season", ch_process_level)
+    return(tmp_df_fls)
+  })
+  
+  df_sls_fls <- Reduce(function(...) merge(..., by = c(1, 2, 3), sort = FALSE), 
+                       ls_df_sls_fls)
+  
+  return(df_sls_fls)
+}
