1c6af88466275376eeed8e468d832e16eb4ab78a
fdetsch <florian.detsch@staff.uni-marburg.de>
2015-05-27 10:19:12 +0200

    Added source scripts containing required packages and functions.

---
+++ R/slsFcts.R
@@ -0,0 +1,2 @@
+fun <- c("R/slsPlots.R", "R/slsAvlFls.R", "R/slsDiurnalVariation.R")
+jnk <- sapply(fun, source)
---
+++ R/slsPkgs.R
@@ -0,0 +1,2 @@
+lib <- c("lubridate", "ggplot2")
+jnk <- sapply(lib, function(i) library(i, character.only = TRUE))
