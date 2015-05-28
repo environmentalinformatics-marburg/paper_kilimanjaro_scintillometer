e478da486ab7ead1cb276b0becc39b7faf7ba354
fdetsch <florian.detsch@staff.uni-marburg.de>
2015-05-28 17:11:03 +0200

    Extended list of required packages.

--- R/slsPkgs.R
+++ R/slsPkgs.R
@@ -1,3 +1,3 @@
 lib <- c("doParallel", "caret", "lubridate", "plotrix", "dplyr", 
-         "reshape2", "ggplot2", "latticeExtra", "gridExtra")
+         "reshape2", "scales", "ggplot2", "latticeExtra", "gridExtra")
 jnk <- sapply(lib, function(i) library(i, character.only = TRUE))
