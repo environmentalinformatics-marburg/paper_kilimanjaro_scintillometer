53707749452783a2e2b136e64a0701de1788c57f
fdetsch <florian.detsch@staff.uni-marburg.de>
2015-05-29 16:54:29 +0200

    **plyr** is now attached prior to **dplyr** (as officially suggested).

--- R/slsPkgs.R
+++ R/slsPkgs.R
@@ -1,3 +1,4 @@
-lib <- c("doParallel", "caret", "lubridate", "plotrix", "dplyr", 
+lib <- c("doParallel", "caret", "lubridate", "plotrix", "plyr", "dplyr", 
          "reshape2", "scales", "ggplot2", "latticeExtra", "gridExtra")
-jnk <- sapply(lib, function(i) library(i, character.only = TRUE))
+jnk <- sapply(lib, function(i) library(i, character.only = TRUE, 
+                                       quietly = TRUE, verbose = FALSE))
