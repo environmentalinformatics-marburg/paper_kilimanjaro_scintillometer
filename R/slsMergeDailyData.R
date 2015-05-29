9d1e4dc04c081797b5ec3fdd1b2669445d0deb54
fdetsch <florian.detsch@staff.uni-marburg.de>
2015-05-29 16:56:46 +0200

    Daily datasets with less than 2 rows are now rejected (e.g. used to be the case on GRA1) prior to data merging.

--- R/slsMergeDailyData.R
+++ R/slsMergeDailyData.R
@@ -1,35 +1,43 @@
 slsMergeDailyData <- function(files, 
                               ...) {
   
-  lib <- c("foreach", "plyr")
-  sapply(lib, function(x) stopifnot(require(x, character.only = TRUE)))
+  ## required packages
+  source("R/slsPkgs.R")
   
+  ## remove day-1 files from gra1
   if (length(grep("gra1", files)) > 0) {
     int_id_20140224 <- grep("2014-02-24", files)
     files <- files[-int_id_20140224]
   }
   
+  ## remove day-1 files from sav5 (dry season)
   if (length(grep("sav5", files)) > 0 & length(grep("2014-09", files)) > 0) {
     int_id_20140912 <- grep("2014-09-12", files)[1]
     files <- files[int_id_20140912:length(files)]
   }
   
-  dat <- foreach(i = files, j = seq(files)) %do% {
+  ## import and merge data
+  ls_df <- foreach(i = files) %do% {
     
-    # Import headers
+    ## import headers
     hdr <- readLines(i)
     index <- grep("#", hdr)
     hdr <- hdr[index]
     hdr <- sapply(strsplit(hdr, " # "), "[[", 2)
     
-    # Import data
+    ## import data without headers
     tmp <- read.table(i, header = FALSE, skip = index[length(index)] + 1, 
                       sep = "\t", na.strings = "*", stringsAsFactors = FALSE)
     names(tmp) <- hdr
     
-    tmp <- tmp[2:nrow(tmp), ]
+    ## skip the first 5 min of measurement, 
+    ## or return NULL if measurement was shorter than 5 min
+    if (nrow(tmp) < 2)
+      return(NULL)
+    else 
+      tmp <- tmp[2:nrow(tmp), ]
     
-    # Reformat datetime 
+    ## reformat datetime 
     tmp.date <- substr(tmp[, 1], 13, 22)
     tmp.time <- substr(tmp[, 1], 24, 31)
     tmp$datetime <- strftime(strptime(paste(tmp.date, tmp.time), 
@@ -37,34 +45,32 @@ slsMergeDailyData <- function(files,
     
     return(tmp)
   }
+  df <- rbind.fill(ls_df)
   
-  dat <- rbind.fill(dat)
+  ## remove rows with missing timestamp
+  if (length(which(is.na(df$time))) > 0)
+    df <- df[-which(is.na(df$time)), ]
   
-  # Remove rows with time == NA
-  if (length(which(is.na(dat$time))) > 0)
-    dat <- dat[-which(is.na(dat$time)), ]
-  # Order data by datetime
-  dat <- dat[order(dat$datetime), ]
+  ## order by datetime
+  df <- df[order(df$datetime), ]
   
-  # adjust sensor time during sav5 dry season measurement
+  ## adjust sensor time on sav5 (dry season)
   if (length(grep("sav5", files)) > 0 & length(grep("2014-09", files)) > 0) {
-    stopifnot(require(lubridate))
-    tmp_dt <- strptime(dat$datetime, format = "%Y-%m-%d %H:%M:%S")
+    tmp_dt <- strptime(df$datetime, format = "%Y-%m-%d %H:%M:%S")
     tmp_dt$hour <- tmp_dt$hour + 1
     tmp_ch <- as.character(tmp_dt)
-    dat$datetime <- tmp_ch
+    df$datetime <- tmp_ch
   }
   
-  # Continuous time series(i.e., begin 0:00 -> end 23:59)
-  st <- paste(substr(dat$datetime[1], 1, 10), "00:00:00")
-  nd <- paste(substr(dat$datetime[nrow(dat)], 1, 10), "23:59:00")
+  ## merge data with continuous time series (begin 0:00, end 23:59) and return
+  st <- paste(substr(df$datetime[1], 1, 10), "00:00:00")
+  nd <- paste(substr(df$datetime[nrow(df)], 1, 10), "23:59:00")
   
   st.nd <- seq(strptime(st, format = "%Y-%m-%d %H:%M:%S"), 
                strptime(nd, format = "%Y-%m-%d %H:%M:%S"), 60)
   st.nd <- strftime(st.nd, format = "%Y-%m-%d %H:%M:%S")
   
-  dat <- merge(data.frame(datetime = st.nd), dat, all.x = TRUE)
-  
-  # Return merged and continuous time series
-  return(dat)
+  df <- merge(data.frame(datetime = st.nd), df, all.x = TRUE)
+
+  return(df)
 }
