0f3609efd502b15904f499331827b220ee6afa59
fdetsch <florian.detsch@staff.uni-marburg.de>
2015-06-05 12:28:14 +0200

    Updated required functions and packages.

--- R/slsFcts.R
+++ R/slsFcts.R
@@ -1,4 +1,4 @@
 fun <- c("R/slsPlots.R", "R/slsAvlFls.R", "R/slsDiurnalVariation.R", 
          "R/slsAggregate.R", "R/slsMergeDailyData.R", "R/slsFoggy.R", 
-         "R/plotPredictionStats.R")
+         "R/plotPredictionStats.R", "R/mergeGPX.R")
 jnk <- sapply(fun, source)
--- R/slsPkgs.R
+++ R/slsPkgs.R
@@ -1,6 +1,6 @@
 lib <- c("doParallel", "caret", "lubridate", "plotrix", "plyr", "dplyr", 
          "reshape2", "scales", "ggplot2", "latticeExtra", "gridExtra", 
-         "TSdist", "Rsenal")
+         "TSdist", "Rsenal", "OpenStreetMap", "Orcs", "rgeos")
 
 jnk <- sapply(lib, function(i) {
   suppressMessages(library(i, character.only = TRUE))
