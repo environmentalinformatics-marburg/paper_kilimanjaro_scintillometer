76462e7b04254eb84060f61e17c6b68b35e5bf49
fdetsch <florian.detsch@staff.uni-marburg.de>
2015-06-10 15:19:10 +0200

    Completed list of required packages and functions.

--- R/slsFcts.R
+++ R/slsFcts.R
@@ -1,4 +1,5 @@
 fun <- c("R/slsPlots.R", "R/slsAvlFls.R", "R/slsDiurnalVariation.R", 
          "R/slsAggregate.R", "R/slsMergeDailyData.R", "R/slsFoggy.R", 
-         "R/plotPredictionStats.R", "R/mergeGPX.R")
+         "R/plotPredictionStats.R", "R/mergeGPX.R", "R/panel.smoothconts.R", 
+         "R/vpd.R")
 jnk <- sapply(fun, source)
