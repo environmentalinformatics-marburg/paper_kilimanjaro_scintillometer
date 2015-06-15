dee310cc2d09e1aa71bc1ea5055c33f972038fa6
fdetsch <florian.detsch@staff.uni-marburg.de>
2015-06-15 13:34:44 +0200

    Included C++ functions script.

--- R/slsFcts.R
+++ R/slsFcts.R
@@ -1,6 +1,11 @@
+## r functions
 fun <- c("R/slsPlots.R", "R/slsAvlFls.R", "R/slsDiurnalVariation.R", 
          "R/slsAggregate.R", "R/slsMergeDailyData.R", "R/slsFoggy.R", 
          "R/plotPredictionStats.R", "R/mergeGPX.R", "R/panel.smoothconts.R", 
          "R/vpd.R", "R/qcMyd17.R", "R/qcxMyd15.R", "R/slsTemporalRange.R", 
          "R/slsGppIndex.R")
 jnk <- sapply(fun, source)
+
+## c++ functions
+library("Rcpp")
+sourceCpp("src/SlsCppFun.cpp")
