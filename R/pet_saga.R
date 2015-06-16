61d64b3ab60699f43488a2d0679f8e45bfe859ac
fdetsch <flowla88@gmail.com>
2015-06-16 11:05:53 +0200

    Extended 'RSAGA' script.

--- R/pet_saga.R
+++ R/pet_saga.R
@@ -1,11 +1,29 @@
+################################################################################
+### code taken from http://www.r-bloggers.com/rsaga-getting-started/         ###
+################################################################################
+
 library(RSAGA)
 
-# set up rsaga environment:
-work_env <- rsaga.env() 
-work_env 
+## set up 'rsaga' environment (dependent upon OS)
+?rsaga.env
 
-# retrieve list of available libraries
-ch_saga_lib <- rsaga.get.libraries()
+if (Sys.info()[["sysname"]] == "Windows") {
+  work_env <- rsaga.env(path = "C:/Program Files (x86)/SAGA-GIS") 
+} else {
+  work_env <- rsaga.env()
+}
 
-sapply(ch_saga_lib, rsaga.get.modules)
-rsaga.get.modules("grid_filter")
+## list of available libraries
+ch_saga_lib <- rsaga.get.libraries(path = work_env$modules)
+
+## modules in library 'climate_tools'
+?rsaga.get.modules
+ls_modules <- rsaga.get.modules("climate_tools", env = work_env)
+
+## usage of 'pet' module
+?rsaga.get.usage
+rsaga.get.usage("climate_tools", module = 6, env = work_env)
+
+## convert required grid files to saga grid format
+# ?rsaga.import.gdal
+# rsaga.import.gdal('lidar.tif')
