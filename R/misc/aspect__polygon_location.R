2ebcae1c54a37e0df0d8d6aec7d91d61bcc9d11f
fdetsch <florian.detsch@staff.uni-marburg.de>
2015-06-24 17:40:26 +0200

    Miscellaneous functions.

---
+++ R/misc/aspect__polygon_location.R
@@ -0,0 +1,38 @@
+library(MODIS)
+library(plotKML)
+
+## plot center coordinates
+spt_plots <- readOGR(dsn = "../../kilimanjaro/coordinates/coords/", 
+                     layer = "PlotPoles_ARC1960_mod_20140807_final", 
+                     p4s = "+init=epsg:21037")
+spt_plots <- spt_plots[spt_plots$PoleType == "AMP", ]
+
+## aspect from dem
+rst_dem <- raster("../../kilimanjaro/coordinates/coords/DEM_ARC1960_30m_Hemp.tif")
+rst_asp <- terrain(rst_dem, opt = "aspect", unit = "degrees")
+
+## modis data
+MODISoptions(localArcPath = "/media/fdetsch/XChange/kilimanjaro/evapotranspiration/MODIS_ARC", 
+             outDirPath = "/media/fdetsch/XChange/kilimanjaro/evapotranspiration/MODIS_ARC/PROCESSED")
+runGdal("MOD15A2", begin = "2015-01-01", end = "2015-01-02", tileH = 21, 
+        tileV = 9, SDSstring = "010000", job = "cell_size", 
+        outProj = "+init=epsg:21037")
+
+## 1-km cells
+ch_dir_cs <- paste(getOption("MODIS_outDirPath"), "cell_size", sep = "/")
+ch_fls_cs <- list.files(ch_dir_cs, pattern = "Lai", full.names = TRUE)
+rst_cs <- raster(ch_fls_cs)
+rst_cs_crp <- crop(rst_cs, raster("../../kilimanjaro/kili_bing.tif"))
+
+for (i in slsPlots()) {
+  spt_plots_sub <- subset(spt_plots, PlotID == i)
+  #   rst_cs_sub <- crop(rst_cs_crp, spt_plots_sub)
+  #   spy_cs_sub <- rasterToPolygons(rst_cs_sub)
+  #   
+  #   ch_fls_kml <- paste0("data/", i, "_1km.kml")
+  #   plotKML(spy_cs_sub, open.kml = FALSE, file.name = ch_fls_kml)
+  
+  # aspect
+  print(paste(i, extract(rst_asp, spt_plots_sub), sep = ", "))
+}
+
