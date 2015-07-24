f299eecdc6a2893989b434a09fd301fc9cf8074f
fdetsch <florian.detsch@staff.uni-marburg.de>
2015-07-24 10:07:50 +0200

    Updated list of required functions.

--- R/slsFcts.R
+++ R/slsFcts.R
@@ -4,7 +4,7 @@ fun <- c("R/slsPlots.R", "R/slsAvlFls.R", "R/slsDiurnalVariation.R",
          "R/plotPredictionStats.R", "R/mergeGPX.R", "R/panel.smoothconts.R", 
          "R/vpd.R", "R/qcMyd17.R", "R/qcxMyd15.R", "R/slsTemporalRange.R", 
          "R/slsGppIndex.R", "R/madjacent.R", "R/summarizeVar.R", 
-         "R/gfPostprocess.R")
+         "R/gfPostprocess.R", "R/visKili.R")
 jnk <- sapply(fun, source)
 
 ## c++ functions
