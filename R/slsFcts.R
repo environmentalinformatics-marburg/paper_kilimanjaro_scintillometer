5426c804faa089bd62498a5676f253d3bf6d459d
fdetsch <florian.detsch@staff.uni-marburg.de>
2016-02-01 17:49:53 +0100

    Minor adjustments to visualization of supplementary data; added helper function to modify strip text in ggplot.

--- R/slsFcts.R
+++ R/slsFcts.R
@@ -10,3 +10,28 @@ jnk <- sapply(fun, source)
 ## c++ functions
 library("Rcpp")
 sourceCpp("src/SlsCppFun.cpp")
+
+## strip labeller (required in visMetData)
+## (found on http://stackoverflow.com/questions/11979017/changing-facet-label-to-math-formula-in-ggplot2)
+facet_wrap_labeller <- function(gg.plot,labels=NULL) {
+  #works with R 3.0.1 and ggplot2 0.9.3.1
+  require(gridExtra)
+  
+  g <- ggplotGrob(gg.plot)
+  gg <- g$grobs      
+  strips <- grep("strip_t", names(gg))
+  
+  for(ii in seq_along(labels))  {
+    modgrob <- getGrob(gg[[strips[ii]]], "strip.text", 
+                       grep=TRUE, global=TRUE)
+    gg[[strips[ii]]]$children[[modgrob$name]] <- editGrob(modgrob,label=labels[ii])
+  }
+  
+  print.arrange <- function(x){
+    grid::grid.draw(x)
+  }
+  
+  g$grobs <- gg
+  class(g) = c("arrange", "ggplot",class(g)) 
+  g
+}
--- R/visMetData.R
+++ R/visMetData.R
@@ -2,10 +2,17 @@ visMetData <- function(process_level = "mrg",
                        path_out = "../../phd/scintillometer/out/plot_data/", 
                        ...) {
   
+  #   try(detach("package:ggplot2", unload = TRUE))
+  #   install.packages("inst/packages/ggplot2_1.0.1.tar.gz", repos = NULL)
+  
   ## required packages and functions
   source("R/slsPkgs.R")
   source("R/slsFcts.R")
   df_sls <- slsAvlFls(...)
+  
+  ## sort by elevation
+  id <- sapply(slsPlots(style = "elevation"), function(i) grep(i, df_sls$plot))
+  df_sls <- df_sls[id, ]
   
   ## visualize meteorological time series per plot
   for (i in 1:nrow(df_sls)) {
@@ -15,24 +22,46 @@ visMetData <- function(process_level = "mrg",
     
     ## import and reformat data
     df <- read.csv(df_sls[i, process_level])
+    df <- df[, -grep("soilHeatFlux", names(df))]
+    df <- df[, -grep("hour", names(df))]
     df_mlt <- melt(df, id.vars = 1)
     df_mlt$datetime <- strptime(df_mlt$datetime, format = "%Y-%m-%d %H:%M:%S")
     
     ## visualization
     p <- ggplot(aes(x = datetime, y = value), 
                 data = subset(df_mlt, variable != "waterET")) + 
-      geom_line() + 
-      geom_point(size = .1) + 
+      geom_line(size = .7) + 
       facet_wrap(~ variable, ncol = 2, scales = "free_y") + 
-      labs(title = paste0(ch_plot, "\n"), x = "\nTime (min)", y = "") + 
+      labs(x = "\nTime (hr)", y = "") + 
       theme_bw() + 
-      theme(text = element_text(size = 14), axis.text = element_text(size = 12))
+      theme(text = element_text(size = 10), axis.text = element_text(size = 7), 
+            strip.text = element_text(face = "bold"))
     
-    ch_fls_out <- paste(ch_plot, ch_ssn, process_level, sep = "_")
-    ch_fls_out <- paste0(path_out, "/", ch_fls_out, ".png")
-    png(ch_fls_out, width = 30, height = 30, units = "cm", res = 300, 
-        pointsize = 18)
-    print(p)
+    lbl <- c(expression("T"["a-200"] ~ "(" * degree * "C)"), 
+             expression("T"["a-20"] ~ "(" * degree * "C)"), 
+             expression("R"["dwn"] ~ "(W/m"^2 * ")"), 
+             expression("R"["up"] ~ "(W/m"^2 * ")"), 
+             "rH (%)", "p (Pa)", "VPD", "Rainfall (mm)")
+    
+    p_upd <- facet_wrap_labeller(p, lbl)
+    
+    # ch_fls_out <- paste(ch_plot, ch_ssn, process_level, sep = "_")
+    ch_fls_out <- paste0("figure_a-", formatC(i, width = 2, flag = 0), ".png")
+    ch_fls_out <- paste0(path_out, "/", ch_fls_out)
+    png(ch_fls_out, width = 20, height = 16, units = "cm", res = 500)
+    grid.newpage()
+    
+    vp_fig <- viewport(x = 0, y = .1, just = c("left", "bottom"), 
+                       width = 1, height = .9)
+    pushViewport(vp_fig)
+    print(p_upd)
+    
+    upViewport(0)
+    vp_cap <- viewport(x = .5, y = .07, width = 1, height = .1)
+    pushViewport(vp_cap)
+    grid.text(bquote(atop(bold("Figure A-") * bold(.(i)) * ". Meteorological data from" ~ .(ch_plot) * ".")), 
+              gp = gpar(cex = .8))
+    
     dev.off()
   }
   
