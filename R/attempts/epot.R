8b1a96177f2a419056c487acf2add01a9bc70b55
fdetsch <florian.detsch@staff.uni-marburg.de>
2017-01-25 12:35:25 +0100

    Update: calculation of mean daily actual and potential ET (incl. standard errors) and subsequent creation of LaTeX output table.

--- R/attempts/epot.R
+++ R/attempts/epot.R
@@ -10,8 +10,12 @@ source("R/slsFcts.R")
 
 ### processing -----
 
-fls <- slsAvlFls()$mrg_rf_agg01h
-ssn <- slsAvlFls()$season
+dsn <- switch(Sys.info()[["sysname"]], 
+              "Windows" = "E:/phd/scintillometer/data", 
+              "Linux" = "/media/permanent/phd/scintillometer/data")
+
+fls <- slsAvlFls(dsn)$mrg_rf_agg01h
+ssn <- slsAvlFls(dsn)$season
 
 dat_epot <- foreach(i = fls, j = ssn, .combine = "rbind") %do% {
   dat <- read.csv(i)
@@ -41,3 +45,30 @@ xyplot(Epot ~ waterET | PlotID, data = dat_epot, panel = function(x, y, ...) {
   panel.xyplot(x, y, col = "grey50")
   panel.ablineq(lm(y ~ x), rotate = TRUE, r.squared = TRUE, at = .8)
 })
+
+
+### -----
+
+dat_epot %>% 
+  group_by(PlotID) %>%
+  summarise(waterET.se = round(plotrix::std.error(waterET, na.rm = TRUE), 2), 
+            Epot.se = round(plotrix::std.error(Epot, na.rm = TRUE), 2), 
+            waterET = round(mean(waterET, na.rm = TRUE), 2), 
+            Epot = round(mean(Epot, na.rm = TRUE), 2)) %>%
+  mutate(Difference = Epot - waterET) %>%
+  data.frame() -> potET
+
+potET$Season <- sapply(strsplit(as.character(potET$PlotID), ", "), "[[", 2)
+potET$PlotID <- sapply(strsplit(as.character(potET$PlotID), ", "), "[[", 1)
+
+potET$waterET <- paste(potET$waterET, potET$waterET.se, sep = " +/- ")
+potET$Epot <- paste(potET$Epot, potET$Epot.se, sep = " +/- ")
+potET <- potET[, c(1, 7, 4, 5, 6)]
+
+ids <- unlist(
+  sapply(rev(slsPlots("elevation")), function(i) grep(i, potET$PlotID))
+)
+potET <- potET[ids, ]
+
+library(stargazer)
+stargazer(potET, summary = FALSE, rownames = FALSE)
